d1e560ab8e18557625dfbdf61af2009d46494823
jacobthewest <jacobthewewst@gmail.com>
2020-09-28 18:02:57 -0600

    The feed appears, just need to correctly bind the username to the status. And also look at the images with the posts

---
+++ app/src/main/java/edu/byu/cs/tweeter/view/main/feed/FeedFragment.java
@@ -0,0 +1,448 @@
+package edu.byu.cs.tweeter.view.main.feed;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import edu.byu.cs.tweeter.R;
+import edu.byu.cs.tweeter.model.domain.AuthToken;
+import edu.byu.cs.tweeter.model.domain.Status;
+import edu.byu.cs.tweeter.model.domain.User;
+import edu.byu.cs.tweeter.model.service.request.FeedRequest;
+import edu.byu.cs.tweeter.model.service.response.FeedResponse;
+import edu.byu.cs.tweeter.presenter.FeedPresenter;
+import edu.byu.cs.tweeter.util.DatePrinter;
+import edu.byu.cs.tweeter.view.asyncTasks.GetFeedTask;
+import edu.byu.cs.tweeter.view.util.ImageUtils;
+
+/**
+ * The fragment that displays on the 'Feed' tab.
+ */
+public class FeedFragment extends Fragment implements FeedPresenter.View {
+
+    private static final String LOG_TAG = "FollowingFragment";
+    private static final String USER_KEY = "UserKey";
+    private static final String AUTH_TOKEN_KEY = "AuthTokenKey";
+
+    private static final int LOADING_DATA_VIEW = 0;
+    private static final int ITEM_VIEW = 1;
+
+    private static final int PAGE_SIZE = 10;
+
+    private User user;
+    private AuthToken authToken;
+    private FeedPresenter presenter;
+
+    private FeedFragment.FeedRecyclerViewAdapter feedRecyclerViewAdapter;
+
+    /**
+     * Creates an instance of the fragment and places the user and auth token in an arguments
+     * bundle assigned to the fragment.
+     *
+     * @param user the logged in user.
+     * @param authToken the auth token for this user's session.
+     * @return the fragment.
+     */
+    public static FeedFragment newInstance(User user, AuthToken authToken) {
+        FeedFragment fragment = new FeedFragment();
+
+        Bundle args = new Bundle(2);
+        args.putSerializable(USER_KEY, user);
+        args.putSerializable(AUTH_TOKEN_KEY, authToken);
+
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_feed, container, false);
+
+        //noinspection ConstantConditions
+        user = (User) getArguments().getSerializable(USER_KEY);
+        authToken = (AuthToken) getArguments().getSerializable(AUTH_TOKEN_KEY);
+
+        presenter = new FeedPresenter(this);
+
+        RecyclerView feedRecyclerView = view.findViewById(R.id.feedRecyclerView);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this.getContext());
+        feedRecyclerView.setLayoutManager(layoutManager);
+
+        feedRecyclerViewAdapter = new FeedFragment.FeedRecyclerViewAdapter();
+        feedRecyclerView.setAdapter(feedRecyclerViewAdapter);
+
+        feedRecyclerView.addOnScrollListener(new FeedFragment.FeedRecyclerViewPaginationScrollListener(layoutManager));
+
+        return view;
+    }
+
+    /**
+     * The ViewHolder for the RecyclerView that displays the Following data.
+     */
+    private class FeedHolder extends RecyclerView.ViewHolder {
+
+        private final ImageView userImage;
+        private final TextView userAlias;
+        private final TextView userName;
+        private TextView postText;
+        private final TextView imageUrl;
+        private final TextView videoUrl;
+        private TextView timePosted;
+        private TextView mentions;
+
+
+        /**
+         * Creates an instance and sets an OnClickListener for the user's row.
+         *
+         * @param itemView the view on which the user will be displayed.
+         */
+        FeedHolder(@NonNull View itemView) {
+            super(itemView);
+
+            userImage = itemView.findViewById(R.id.userImage);
+            userAlias = itemView.findViewById(R.id.userAlias);
+            userName = itemView.findViewById(R.id.userName);
+            postText = itemView.findViewById(R.id.postText);
+            imageUrl = itemView.findViewById(R.id.imageUrl);
+            videoUrl = itemView.findViewById(R.id.videoUrl);
+            timePosted = itemView.findViewById(R.id.timePosted);
+            mentions = itemView.findViewById(R.id.mentions);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(getContext(), "You selected '" + userName.getText() + "'.", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        /**
+         * Binds the status's data to the view.
+         *
+         * @param status the status.
+         */
+        void bindStatus(Status status) {
+            String tempPostText = formulatePostText(status);
+            String tempTimePosted = formulateTimePosted(status.getTimePosted());
+
+            userImage.setImageDrawable(ImageUtils.drawableFromByteArray(user.getImageBytes()));
+            userAlias.setText(user.getAlias());
+            userName.setText(user.getName());
+            postText.setText(tempPostText);
+            timePosted.setText(tempTimePosted);
+        }
+
+        /**
+         * Adds links to the mentions, and video/image URLs.
+         * @param status
+         * @return A string with clickable links
+         */
+        private String formulatePostText(Status status) {
+            Writer out = new StringWriter();
+            String imageUrl = "";
+            String videoUrl = "";
+
+            if(!status.getImageUrl().equals("") && !status.getImageUrl().equals(null)) {
+                imageUrl = "\nImage URL: " + status.getImageUrl();
+            }
+
+            if(!status.getVideoUrl().equals("") && !status.getVideoUrl().equals(null)) {
+                videoUrl = "\nVideo URL: " + status.getVideoUrl();
+            }
+
+            try {
+                out.write(status.getPostText() + imageUrl + videoUrl + status.getVideoUrl());
+                boolean mentionsPrinted = false;
+                List<String> mentions = status.getMentions();
+                for(int i = 0; i < mentions.size(); i++) {
+                    if(!mentionsPrinted && (!mentions.get(i).equals("") && !mentions.get(i).equals(null))) {
+                        out.write("\nMentions: ");
+                        mentionsPrinted = true;
+                    }
+                    out.write(status.getMentions().get(i) + " ");
+                }
+            } catch(Exception e) {
+                return "Error in FeedFragment.FormulatePostText()";
+            }
+            return out.toString();
+        }
+
+        /**
+         * Makes the date into the correct string format
+         * @param cal
+         * @return A readable date as a string
+         */
+        private String formulateTimePosted(Calendar cal) {
+            DatePrinter datePrinter = new DatePrinter(cal);
+            return datePrinter.toString();
+        }
+    }
+
+    /**
+     * The adapter for the RecyclerView that displays the Following data.
+     */
+    private class FeedRecyclerViewAdapter extends RecyclerView.Adapter<FeedFragment.FeedHolder> implements GetFeedTask.Observer {
+
+        private final List<Status> feed = new ArrayList<>();
+
+        private edu.byu.cs.tweeter.model.domain.Status lastStatus;
+
+        private boolean hasMorePages;
+        private boolean isLoading = false;
+
+        /**
+         * Creates an instance and loads the first page of following data.
+         */
+        FeedRecyclerViewAdapter() {
+            loadMoreItems();
+        }
+
+        /**
+         * Adds new statuses to the list from which the RecyclerView retrieves the statuses it displays
+         * and notifies the RecyclerView that items have been added.
+         *
+         * @param newStatuses the statuses to add.
+         */
+        void addItems(List<Status> newStatuses) {
+            int startInsertPosition = feed.size();
+            feed.addAll(newStatuses);
+            this.notifyItemRangeInserted(startInsertPosition, newStatuses.size());
+        }
+
+        /**
+         * Adds a single status to the list from which the RecyclerView retrieves the statuses it
+         * displays and notifies the RecyclerView that an item has been added.
+         *
+         * @param status the status to add.
+         */
+        void addItem(Status status) {
+            feed.add(status);
+            this.notifyItemInserted(feed.size() - 1);
+        }
+
+        /**
+         * Removes a user from the list from which the RecyclerView retrieves the users it displays
+         * and notifies the RecyclerView that an item has been removed.
+         *
+         * @param status the status to remove.
+         */
+        void removeItem(Status status) {
+            int position = feed.indexOf(status);
+            feed.remove(position);
+            this.notifyItemRemoved(position);
+        }
+
+        /**
+         *  Creates a view holder for a status to be displayed in the RecyclerView or for a message
+         *  indicating that new rows are being loaded if we are waiting for rows to load.
+         *
+         * @param parent the parent view.
+         * @param viewType the type of the view (ignored in the current implementation).
+         * @return the view holder.
+         */
+        @NonNull
+        @Override
+        public FeedFragment.FeedHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(FeedFragment.this.getContext());
+            View view;
+
+            if(viewType == LOADING_DATA_VIEW) {
+                view = layoutInflater.inflate(R.layout.loading_row, parent, false);
+
+            } else {
+                view = layoutInflater.inflate(R.layout.status_row, parent, false);
+            }
+
+            return new FeedFragment.FeedHolder(view);
+        }
+
+        /**
+         * Binds the status at the specified position unless we are currently loading new data. If
+         * we are loading new data, the display at that position will be the data loading footer.
+         *
+         * @param feedHolder the ViewHolder to which the status should be bound.
+         * @param position the position (in the list of statuses) that contains the status to be
+         *                 bound.
+         */
+        @Override
+        public void onBindViewHolder(@NonNull FeedFragment.FeedHolder feedHolder, int position) {
+            if(!isLoading) {
+                feedHolder.bindStatus(feed.get(position));
+            }
+        }
+
+        /**
+         * Returns the current number of statuses available for display.
+         * @return the number of statuses available for display.
+         */
+        @Override
+        public int getItemCount() {
+            return feed.size();
+        }
+
+        /**
+         * Returns the type of the view that should be displayed for the item currently at the
+         * specified position.
+         *
+         * @param position the position of the items whose view type is to be returned.
+         * @return the view type.
+         */
+        @Override
+        public int getItemViewType(int position) {
+            return (position == feed.size() - 1 && isLoading) ? LOADING_DATA_VIEW : ITEM_VIEW;
+        }
+
+        /**
+         * Causes the Adapter to display a loading footer and make a request to get more following
+         * data.
+         */
+        void loadMoreItems() {
+            isLoading = true;
+            addLoadingFooter();
+
+            GetFeedTask getFeedTask = new GetFeedTask(presenter, this);
+            FeedRequest request = new FeedRequest(user, PAGE_SIZE, lastStatus);
+            getFeedTask.execute(request);
+        }
+
+        /**
+         * A callback indicating more following data has been received. Loads the new statuses
+         * and removes the loading footer.
+         *
+         * @param feedResponse the asynchronous response to the request to load more items.
+         */
+        @Override
+        public void statusesRetrieved(FeedResponse feedResponse) {
+            List<Status> statuses = feedResponse.getFeed();
+
+            lastStatus = (statuses.size() > 0) ? statuses.get(statuses.size() -1) : null;
+            hasMorePages = feedResponse.getHasMorePages();
+
+            isLoading = false;
+            removeLoadingFooter();
+            feedRecyclerViewAdapter.addItems(statuses);
+        }
+
+        /**
+         * A callback indicating that an exception was thrown by the presenter.
+         *
+         * @param exception the exception.
+         */
+        @Override
+        public void handleException(Exception exception) {
+            Log.e(LOG_TAG, exception.getMessage(), exception);
+            removeLoadingFooter();
+            Toast.makeText(getContext(), exception.getMessage(), Toast.LENGTH_LONG).show();
+        }
+
+        /**
+         * Adds a dummy user to the list of users so the RecyclerView will display a view (the
+         * loading footer view) at the bottom of the list.
+         */
+        private void addLoadingFooter() {
+            List<String> mentions = getMentions();
+            Calendar timePosted = getTimePosted();
+            String imageUrl = "https://preview.tinyurl.com/yxrxp5d2";
+            String videoUrl = "https://youtu.be/oHg5SJYRHA0";
+            String postUrl = "Statuses are loading";
+            addItem(new Status(new User("Dummy", "User", ""), postUrl, imageUrl, videoUrl, timePosted, mentions));
+        }
+
+        /**
+         * Removes the dummy status from the list of statuses so the RecyclerView will stop displaying
+         * the loading footer at the bottom of the list.
+         */
+        private void removeLoadingFooter() {
+            removeItem(feed.get(feed.size() - 1));
+        }
+
+        /**
+         * Generates a mention for the addLoadingFooter function
+         * @return A list of mentions
+         */
+        private List<String> getMentions() {
+            List<String> mentions = new ArrayList<>();
+            mentions.add("@TestMention");
+            mentions.add("@TheRealSlimShady");
+            return mentions;
+        }
+
+        /**
+         * Generates a Date for the addLoadingFooter function
+         * @return A date object
+         */
+        private Calendar getTimePosted() {
+            Calendar c1 = Calendar.getInstance();
+            c1.set(Calendar.MONTH, 6);
+            c1.set(Calendar.DATE, 11);
+            c1.set(Calendar.YEAR, 2020);
+            c1.set(Calendar.HOUR_OF_DAY, 10); // 24 hours
+            return c1;
+        }
+    }
+
+    /**
+     * A scroll listener that detects when the user has scrolled to the bottom of the currently
+     * available data.
+     */
+    private class FeedRecyclerViewPaginationScrollListener extends RecyclerView.OnScrollListener {
+
+        private final LinearLayoutManager layoutManager;
+
+        /**
+         * Creates a new instance.
+         *
+         * @param layoutManager the layout manager being used by the RecyclerView.
+         */
+        FeedRecyclerViewPaginationScrollListener(LinearLayoutManager layoutManager) {
+            this.layoutManager = layoutManager;
+        }
+
+        /**
+         * Determines whether the user has scrolled to the bottom of the currently available data
+         * in the RecyclerView and asks the adapter to load more data if the last load request
+         * indicated that there was more data to load.
+         *
+         * @param recyclerView the RecyclerView.
+         * @param dx the amount of horizontal scroll.
+         * @param dy the amount of vertical scroll.
+         */
+        @Override
+        public void onScrolled(@NotNull RecyclerView recyclerView, int dx, int dy) {
+            super.onScrolled(recyclerView, dx, dy);
+
+            int visibleItemCount = layoutManager.getChildCount();
+            int totalItemCount = layoutManager.getItemCount();
+            int firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition();
+
+            if (!feedRecyclerViewAdapter.isLoading && feedRecyclerViewAdapter.hasMorePages) {
+                if ((visibleItemCount + firstVisibleItemPosition) >=
+                        totalItemCount && firstVisibleItemPosition >= 0) {
+                    feedRecyclerViewAdapter.loadMoreItems();
+                }
+            }
+        }
+    }
+}
+
+
